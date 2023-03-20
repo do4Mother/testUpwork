2883c841e44518782ed015e97192c4d656dad001
balt1794 <bal22@njit.edu>
2023-03-20 11:44:03 -0400

    Fixed LoadingDots

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -1,14 +1,13 @@
 import styles from "../styles/loading-dots.module.css";
 
-const LoadingDots = ({
-  color = "#000",
-  style = "small",
-}: {
-  color: string;
-  style: string;
-}) => {
+type LoadingDotsProps = {
+  color?: string;
+  style?: "small" | "large";
+};
+
+const LoadingDots = ({ color = "#000", style = "small" }: LoadingDotsProps) => {
   return (
-    <span className={style == "small" ? styles.loading2 : styles.loading}>
+    <span className={style === "small" ? styles.loading2 : styles.loading}>
       <span style={{ backgroundColor: color }} />
       <span style={{ backgroundColor: color }} />
       <span style={{ backgroundColor: color }} />
@@ -17,7 +16,3 @@ const LoadingDots = ({
 };
 
 export default LoadingDots;
-
-LoadingDots.defaultProps = {
-  style: "small",
-};
