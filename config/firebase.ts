1db408bbdfb1712a2b1d8e32dd3e96107d06bc66
balt1794 <bal22@njit.edu>
2023-03-20 21:44:37 -0400

    Login and Registration Added

---
+++ config/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVa0A_y-0RBHXzhoQULNbpQil5PGihMzw",
+  authDomain: "next-firebase-auth-44ef9.firebaseapp.com",
+  projectId: "next-firebase-auth-44ef9",
+  storageBucket: "next-firebase-auth-44ef9.appspot.com",
+  messagingSenderId: "81320803444",
+  appId: "1:81320803444:web:f3d74800b5271f71a5a521"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Navbar from "../components/Navbar";
+import ProtectedRoute from "../components/ProtectedRoute";
+import { AuthContextProvider } from "../context/AuthContext";
 import "../styles/globals.css";
 
+const noAuthRequired = ['/', '/login', '/signup']
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   return (
     <>
+    <AuthContextProvider>
+      <Navbar/>
+      {noAuthRequired.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ):(
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
       <Component {...pageProps} />
       <Analytics />
+    </AuthContextProvider>
     </>
   );
 }
