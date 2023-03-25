513c0126d4dc5d9d0ecc65b74c40e4cd4fbf7bd3
balt1794 <bal22@njit.edu>
2023-03-25 17:27:57 -0400

    Feedback box

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
+     
       <Link
+          target="_blank"
           href="https://twitter.com/balt1794"
           className="group"
           aria-label="TaxPal on Twitter"
@@ -15,14 +17,22 @@ export default function Footer() {
           >
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
           </svg>
+          
         </Link>
       
    
       </div>
+      <Link
+          target="_blank"
+          href="https://twitter.com/balt1794"
+          className="group"
+          aria-label="TaxPal on Twitter"
+        >
       <div className="flex space-x-4 pb-4 sm:pb-0">
       
-      
+      <p className="text-gray-800 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-black">Feedback  📩</p>
       </div>
+      </Link>
     </footer>
   );
 }
