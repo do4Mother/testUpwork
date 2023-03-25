43e35ba06844a3c97575a6f4ad41ff5da04c47fb
balt1794 <bal22@njit.edu>
2023-03-25 16:10:05 -0400

    Mobile working now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,22 +14,14 @@ const Navbar = () => {
     <div className="relative flex h-16 items-center justify-between">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
       
-        <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-         
-          <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-         
-          <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
+      <Link className="hover:scale-105" href="/" passHref>
+        <img className="block h-8 w-auto lg:hidden " src="/rbai.png" alt="Your Company"></img>
+        </Link>
       </div>
       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div className="flex flex-shrink-0 items-center ">
         <Link className="hover:scale-105" href="/" passHref>
-          <img className="block h-8 w-auto lg:hidden " src="/rbai.png" alt="Your Company"></img>
+          
           <img className="hidden h-8 w-auto lg:block lg:h-14" src="/rbai.png" alt="Your Company"></img>
           </Link>
         </div>
