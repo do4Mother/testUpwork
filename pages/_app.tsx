da03cee5aceb57652318f52c7d14731b319d32da
balt1794 <bal22@njit.edu>
2023-03-26 10:50:17 -0400

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { AuthContextProvider } from "../context/AuthContext";
 import "../styles/globals.css";
 import 'fontsource-nunito'
 import { useEffect } from "react";
+import Script from 'next/script'
 
 const noAuthRequired = ['/', '/login', '/signup']
 
@@ -19,7 +20,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   return (
     <>
-
+<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-23E0PEG895"/>
+<Script
+  id='google-analytics'
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+__html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-23E0PEG895', {
+  page_path: window.location.pathname,
+ });
+`,
+}}
+ />
     <AuthContextProvider>
       <Navbar/>
       {noAuthRequired.includes(router.pathname) ? (
